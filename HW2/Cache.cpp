32446cf8f94bc06eb6ced78dc26b324cff5b585e
Nadav David <nadav.david@campus.technion.ac.il>
2021-12-04 16:42:28 +0200

    Add HW2. Almost finished (example 3 unclear)
    Co-authored-by: roilatzres <roilatzres@users.noreply.github.com>

---
+++ HW2/Cache.cpp
@@ -0,0 +1,132 @@
+#include <vector>
+#include <math.h>
+
+using std::vector;
+using std::cout;
+using std::endl;
+
+#define ADDRESS_SIZE 32
+
+typedef struct TagEntry
+{
+    unsigned tag;
+    unsigned addr;
+    bool dirty;
+} TagEntry;
+
+
+//Helper method to extract bits from given number
+unsigned get_bits(unsigned number, int start, int N_bits){
+	unsigned n = number; // n = ZZZZZ YYYY 00
+	n = n >> start; //    	ZZZZZ YYYY 00 => ZZZZZ YYYY
+	unsigned mask = 1 << N_bits; // mask = 00001 0000
+	mask -= 1; // 0001000 => 0000111
+	return n & mask;
+}
+
+class Cache
+{
+private:
+    int size;
+    int N_ways_bits;
+    int block_size;
+    bool is_write_allocate;
+    vector< vector<TagEntry> > cache_table;
+
+    //bits Calculations
+    int N_blocks;
+    int N_sets;
+    int offset_bits;
+    int set_bits;
+    int tag_bits;
+    int N_ways;
+public:
+    Cache(int size, int N_ways_bits, int block_size, unsigned access_time, bool is_write_allocate);
+    bool seek(unsigned long int addr, char op, bool writeback);
+    bool add_address(unsigned long int addr, unsigned long int* dirty_tag, bool* removed);
+    void remove(unsigned long int addr);
+    double N_misses, N_hits;
+    unsigned access_time;
+};
+
+Cache::Cache(int size, int N_ways_bits, int block_size, unsigned access_time, bool is_write_allocate) :
+                size(size), N_ways_bits(N_ways_bits), block_size(block_size), access_time(access_time), is_write_allocate(is_write_allocate), N_misses(0), N_hits(0){
+        
+        this->N_ways = pow(2, this->N_ways_bits);
+
+        this->N_blocks = pow(2, this->size) / pow(2, this->block_size);
+        this->N_sets = N_blocks / this->N_ways;
+        this->offset_bits = this->block_size;
+        this->set_bits = log2(N_sets); //Can also be size - block_size - N_ways (in bits)
+        this->tag_bits = ADDRESS_SIZE - set_bits - offset_bits;
+
+        this->cache_table = vector< vector<TagEntry> >(N_sets);
+}
+
+bool Cache::seek(unsigned long int addr, char op, bool writeback){
+    unsigned addr_set = get_bits(addr, this->offset_bits, this->set_bits);
+    unsigned addr_tag = get_bits(addr, this->offset_bits + this->set_bits, this->tag_bits);
+
+    cout << "SET: " << addr_set << ", TAG: " << addr_tag << endl;
+
+    for (vector<TagEntry>::iterator it = this->cache_table[addr_set].begin();
+         it != this->cache_table[addr_set].end(); it++){
+        if (it->tag == addr_tag){
+            TagEntry new_entry;
+            new_entry.tag = it->tag;
+            new_entry.dirty = it->dirty;
+            new_entry.addr = it->addr;
+            
+            this->cache_table[addr_set].erase(it);
+            
+            this->cache_table[addr_set].insert(this->cache_table[addr_set].begin(), new_entry);
+            if (op == 'w')
+                this->cache_table[addr_set].begin()->dirty = true;
+
+            if (!writeback){
+                this->N_hits++;
+            }
+            return true;
+        }
+    }
+    // cout << "Misses: " << this->N_misses << "Hits: " << this->N_hits << endl;
+    if (!writeback){
+        this->N_misses++;
+    }
+    return false;
+}
+
+bool Cache::add_address(unsigned long int addr, unsigned long int* removed_addr, bool* removed){
+    *removed = false;
+    unsigned addr_set = get_bits(addr, this->offset_bits, this->set_bits);
+    unsigned addr_tag = get_bits(addr, this->offset_bits + this->set_bits, this->tag_bits);
+
+    TagEntry new_entry;
+    new_entry.tag = addr_tag;
+    new_entry.dirty = false;
+    new_entry.addr = addr;
+
+    this->cache_table[addr_set].insert(this->cache_table[addr_set].begin(), new_entry);
+    if (this->cache_table[addr_set].size() > this->N_ways){
+        *removed = true;
+        TagEntry to_delete = *(this->cache_table[addr_set].end()--);
+        this->cache_table[addr_set].pop_back();
+        *removed_addr = to_delete.addr;
+        if (to_delete.dirty)
+            return true;
+        return false;
+    }
+    return false;
+}
+
+void Cache::remove(unsigned long int addr) {
+    unsigned addr_set = get_bits(addr, this->offset_bits, this->set_bits);
+    unsigned addr_tag = get_bits(addr, this->offset_bits + this->set_bits, this->tag_bits);
+    for (vector<TagEntry>::iterator it = this->cache_table[addr_set].begin();
+         it != this->cache_table[addr_set].end(); it++){
+        if (it->tag == addr_tag){
+            this->cache_table[addr_set].erase(it);
+            return;
+        }
+    }
+}
---
+++ HW2/cacheSim.cpp
@@ -0,0 +1,198 @@
+/* 046267 Computer Architecture - Winter 20/21 - HW #2 */
+
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "Cache.cpp"
+
+using std::FILE;
+using std::string;
+using std::cout;
+using std::endl;
+using std::cerr;
+using std::ifstream;
+using std::stringstream;
+
+
+double handle_line(Cache& L1, Cache& L2, char op, unsigned long int addr, unsigned MemCyc, bool WrAlloc){ //CHECK: unsigned -> bool conversion
+	
+	unsigned long int removed_addr = 0;
+	bool removed = false;
+
+	if (op == 'r'){
+		bool L1_hit = L1.seek(addr, op, false);
+		//L1 Hit
+		if (L1_hit)
+		{
+			cout << "L1 HIT" << endl;
+			return L1.access_time;
+		}
+		cout << "L1 MISS" << endl;
+		//L1 Miss
+		bool L2_hit = L2.seek(addr, op, false);
+		//L2 Hit
+		if (L2_hit){
+			cout << "L2 HIT" << endl;
+			bool L1_dirty = L1.add_address(addr, &removed_addr, &removed);
+			if (L1_dirty){
+				L2.seek(removed_addr, 'w', true); //CHECK: Does cycle time not change when accessing L2 AGAIN?
+			}
+			return L1.access_time + L2.access_time;
+		}
+		cout << "L2 MISS" << endl;
+		//L2 Miss
+		bool L2_dirty = L2.add_address(addr, &removed_addr, &removed); // L2_dirty unused because this program doesn't handle the memory side. CHECK: DO WE NEED TO ADD T_mem HERE?
+		//IF REMOVED SOMETHING FROM L2 -> SNOOP L1 AND REMOVE IT (+ Check Dirty Stuff)
+		//TODO: Just remove the removed TagEntry from L1 as well (Program doesn't handle "dirty to memory")
+		if (removed){
+			L1.remove(removed_addr);
+		}
+		bool L1_dirty = L1.add_address(addr, &removed_addr, &removed);
+		if (L1_dirty){
+			cout << "DIRTY" << endl;
+			L2.seek(removed_addr, 'w', true); //CHECK: Does cycle time not change when accessing L2 AGAIN?
+		}
+		return L1.access_time + L2.access_time + MemCyc;
+	}
+	else { //if (op == 'w')
+		bool L1_hit = L1.seek(addr, op, false);
+		//L1 Hit
+		if (L1_hit)
+		{
+			cout << "L1 HIT" << endl;
+			return L1.access_time;
+		}
+
+		cout << "L1 MISS" << endl;
+		//L1 Miss
+		bool L2_hit = L2.seek(addr, op, false);
+		//L2 Hit
+		if (L2_hit){
+			cout << "L2 HIT" << endl;
+			if (WrAlloc){
+				bool L1_dirty = L1.add_address(addr, &removed_addr, &removed);
+				if (L1_dirty){
+					L2.seek(removed_addr, 'w', true); //CHECK: Does cycle time not change when accessing L2 AGAIN?
+				}
+			}
+			return L1.access_time + L2.access_time;
+		}
+		cout << "L2 MISS" << endl;
+		//L2 Miss
+		if (WrAlloc){
+			bool L2_dirty = L2.add_address(addr, &removed_addr, &removed); // L2_dirty unused because this program doesn't handle the memory side. CHECK: DO WE NEED TO ADD T_mem HERE?
+			if (removed){
+				L1.remove(removed_addr);
+			}
+			bool L1_dirty = L1.add_address(addr, &removed_addr, &removed);
+			if (L1_dirty){
+				L2.seek(removed_addr, 'w', true); //CHECK: Does cycle time not change when accessing L2 AGAIN?
+			}
+		}
+		return L1.access_time + L2.access_time + MemCyc;
+	}
+}
+
+int main(int argc, char **argv) {
+
+	if (argc < 19) {
+		cerr << "Not enough arguments" << endl;
+		return 0;
+	}
+
+	// Get input arguments
+
+	// File
+	// Assuming it is the first argument
+	char* fileString = argv[1];
+	ifstream file(fileString); //input file stream
+	string line;
+	if (!file || !file.good()) {
+		// File doesn't exist or some other error
+		cerr << "File not found" << endl;
+		return 0;
+	}
+
+	unsigned MemCyc = 0, BSize = 0, L1Size = 0, L2Size = 0, L1Assoc = 0,
+			L2Assoc = 0, L1Cyc = 0, L2Cyc = 0, WrAlloc = 0;
+
+	for (int i = 2; i < 19; i += 2) {
+		string s(argv[i]);
+		if (s == "--mem-cyc") {
+			MemCyc = atoi(argv[i + 1]);
+		} else if (s == "--bsize") {
+			BSize = atoi(argv[i + 1]);
+		} else if (s == "--l1-size") {
+			L1Size = atoi(argv[i + 1]);
+		} else if (s == "--l2-size") {
+			L2Size = atoi(argv[i + 1]);
+		} else if (s == "--l1-cyc") {
+			L1Cyc = atoi(argv[i + 1]);
+		} else if (s == "--l2-cyc") {
+			L2Cyc = atoi(argv[i + 1]);
+		} else if (s == "--l1-assoc") {
+			L1Assoc = atoi(argv[i + 1]);
+		} else if (s == "--l2-assoc") {
+			L2Assoc = atoi(argv[i + 1]);
+		} else if (s == "--wr-alloc") {
+			WrAlloc = atoi(argv[i + 1]);
+		} else {
+			cerr << "Error in arguments" << endl;
+			return 0;
+		}
+	}
+
+
+	Cache L1(L1Size, L1Assoc, BSize, L1Cyc, WrAlloc);
+	Cache L2(L2Size, L2Assoc, BSize, L2Cyc, WrAlloc);
+	double L1MissRate = 0;
+	double L2MissRate = 0;
+	double avgAccTime = 0;
+
+	int counter = 0;
+	double sum_cycles = 0;
+
+	while (getline(file, line)) {
+
+		stringstream ss(line);
+		string address;
+		char operation = 0; // read (R) or write (W)
+		if (!(ss >> operation >> address)) {
+			// Operation appears in an Invalid format
+			cout << "Command Format error" << endl;
+			return 0;
+		}
+
+		// DEBUG - remove this line
+		cout << "operation: " << operation;
+
+		string cutAddress = address.substr(2); // Removing the "0x" part of the address
+
+		// DEBUG - remove this line
+		// cout << ", address (hex)" << cutAddress;
+
+		unsigned long int num = 0;
+		num = strtoul(cutAddress.c_str(), NULL, 16);
+
+		// DEBUG - remove this line
+		cout << " (dec) " << num << endl;
+
+		unsigned long int addr = num;
+		
+		counter++;
+		double current_time = handle_line(L1, L2, operation, addr, MemCyc, WrAlloc);
+		cout << endl << endl;
+		sum_cycles += current_time;
+	}
+
+	avgAccTime = sum_cycles / counter;
+	L1MissRate = L1.N_misses / (L1.N_hits + L1.N_misses);
+	L2MissRate = L2.N_misses / (L2.N_hits + L2.N_misses);
+
+	printf("L1miss=%.03f ", L1MissRate);
+	printf("L2miss=%.03f ", L2MissRate);
+	printf("AccTimeAvg=%.03f\n", avgAccTime);
+
+	return 0;
+}
