9ca46426962736ba2f41ae5dd38a184058255dbf
Nadav David <nadav.david@campus.technion.ac.il>
2022-01-14 12:09:02 +0200

    Add facebook tests for HW4. Finish HW4 (with tar file)

--- HW4/core_api.cpp
+++ HW4/core_api.cpp
@@ -17,7 +17,7 @@ class Thread {
 	int thread_id;
 	tcontext reg_file;
 	bool is_halted;
-	int start_cycle;
+	int start_cycle; // The cycle in which the current thread STARTED running the current instruction
 
 	Thread(int id);
 	bool execute_inst(int start_cycle);
@@ -27,18 +27,19 @@ class Thread {
 	Thread::Thread(int id): inst_address(0), thread_id(id), is_halted(false), start_cycle(0) {
 		current_inst.opcode = CMD_NOP; // For access to latency before setting instruction
 		this->reg_file = tcontext();
+		// Initializing registers
 		for (int i=0; i<REGS_COUNT; i++){
 			this->reg_file.reg[i] = 0;
 		}
 	}
 
 	bool Thread::execute_inst(int start_cycle) {
-		this->start_cycle = start_cycle + 1;
-		SIM_MemInstRead(this->inst_address, &current_inst, this->thread_id);
+		this->start_cycle = start_cycle + 1; // Set the instruction start cycle
+		SIM_MemInstRead(this->inst_address, &current_inst, this->thread_id); // Read next instruction from memory
 		this->execute();
 
 		this->inst_address++;
-		bool event = (this->current_inst.opcode == CMD_LOAD) || (this->current_inst.opcode == CMD_STORE) || (this->current_inst.opcode == CMD_HALT);
+		bool event = (this->current_inst.opcode == CMD_LOAD) || (this->current_inst.opcode == CMD_STORE) || (this->current_inst.opcode == CMD_HALT); // To tell a block MT that it should try to switch threads
 		return event;
 	}
 
@@ -91,7 +92,7 @@ class Thread {
 				offset = this->reg_file.reg[src2];
 			
 			unsigned int address = this->reg_file.reg[dst] + offset;
-			SIM_MemDataWrite(address, src1);
+			SIM_MemDataWrite(address, this->reg_file.reg[src1]);
 			return;
 		}
 		
@@ -110,6 +111,7 @@ class Thread_Handler {
 	int current_thread_id;
 	Thread* current_thread;
 	
+	// Parameters
 	int load_lat;
 	int store_lat;
 	int switch_penalty;
@@ -129,6 +131,7 @@ class Thread_Handler {
 Thread_Handler::Thread_Handler(bool is_blocked_MT, int load_lat, int store_lat, int switch_pen, int N_threads): current_cycle(0), N_instructions(0), is_blocked_MT(is_blocked_MT),
 				current_thread_id(0), load_lat(load_lat), store_lat(store_lat), switch_penalty(switch_pen), N_threads(N_threads){
 	this->threads = vector<Thread>();
+	//Initializing threads
 	for (int i=0; i<this->N_threads; i++){
 		Thread th = Thread(i);
 		this->threads.push_back(th);
@@ -149,7 +152,7 @@ bool Thread_Handler::thread_free(){
 	else if (opcode == CMD_STORE){
 		latency = this->store_lat;
 	}
-	return (this->current_cycle - current_thread->start_cycle >= latency);
+	return (this->current_cycle - current_thread->start_cycle >= latency); // Wheather this thread is open for running its next instruction
 }
 
 int Thread_Handler::latency(int opcode){
@@ -162,16 +165,18 @@ int Thread_Handler::latency(int opcode){
 }
 
 bool Thread_Handler::switch_thread(){
+	// Prioritizing current thread execution in blocked MT
 	if (this->is_blocked_MT && !this->current_thread->is_halted){
 		if (this->current_cycle - this->current_thread->start_cycle >= this->latency(this->current_thread->current_inst.opcode)){
 			return false;
 		}
 	}
+	// Iterating through threads
 	for (int i=1; i<=this->N_threads; i++){
-		Thread* th = &(this->threads[(this->current_thread_id + i) % this->N_threads]);
+		Thread* th = &(this->threads[(this->current_thread_id + i) % this->N_threads]); // Cyclic iteration
 		if (th->is_halted)
 			continue;
-		if (this->current_cycle - th->start_cycle >= this->latency(th->current_inst.opcode)){
+		if (this->current_cycle - th->start_cycle >= this->latency(th->current_inst.opcode)){ // Thread finished running its current instruction
 			this->current_thread = th;
 			this->current_thread_id = (this->current_thread_id + i) % this->N_threads;
 			return false;
@@ -199,12 +204,12 @@ void Thread_Handler::run_program(){
 void Thread_Handler::run_program_finegrained(){
 	bool is_idle = false;
 	while (!this->all_halted()) {
-		if (!is_idle) {
+		if (!is_idle) { // if there's an open thread for running instruction
 			this->current_thread->execute_inst(this->current_cycle);
 			this->N_instructions++;
 		}
 		this->current_cycle++;
-		is_idle = this->switch_thread();
+		is_idle = this->switch_thread(); // Context Switch and check if there's an open thread
 	}
 }
 
@@ -213,49 +218,19 @@ void Thread_Handler::run_program_blocked(){
 	bool event = false;
 	int prev_id = 0;
 	while (!this->all_halted()) {
-		if (!is_idle) {
+		if (!is_idle) { // if there's an open thread for running instruction
 			event = this->current_thread->execute_inst(this->current_cycle);
 			this->N_instructions++;
 		}
 		this->current_cycle++;
-		if (event || is_idle){
+		if (event || is_idle){ // If the current thread is running a memory access instruction (or halted or in idle mode)
 			prev_id = this->current_thread_id;
 			is_idle = this->switch_thread();
-			if (!is_idle && (prev_id != this->current_thread_id))
+			if (!is_idle && (prev_id != this->current_thread_id)) // Only invoke penalty if switched to a DIFFERENT thread
 				this->current_cycle += this->switch_penalty;
 		}
 	}
-	// this->current_cycle++;
 }
-
-/*
-Handler:
-	int cycle (Current Cycle)
-	int N_instructions (Number of executed instructions)
-	bool is_blocked_MT
-	vector<Thread>
-	int current_thread_id
-	Thread* current_thread 
-
-	(From sim_api)
-	int load_lat; 
-	int store_lat;
-	int switch_penalty;
-	int N_threads;
-
-	switch_threads (function) (Advances Thread pointer to next thread)
-
-Thread:
-	Instruction* current_inst
-	int inst_address (each instruction calls SIM_MemInstRead)
-	thread_ID (int)
-	tcontext (Register File)
-	bool is_halted (is finished)
-	int start_cycle (Time since instruction start = Handler.cycle - this.start_cycle)
-
-	perform_next_instruction (has to return opcode)
-
-*/
 
 Thread_Handler blocked_MT;
 Thread_Handler finegrained_MT;
